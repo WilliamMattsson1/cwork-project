972a385ca37fabdd7ae3f282f66c822088e8c866
WilliamMattsson1 <wmattsson@hotmail.com>
2023-09-20 11:52:34 +0200

    Added JS feature (BTN)

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
+/* Add and removes the .open class when clicked */
 function toggleMenu() {
     const menu = document.querySelector('.menu-links')
     const icon = document.querySelector('.hamburger-icon')
     menu.classList.toggle('open')
     icon.classList.toggle('open')
 }
+
+/* Adds message when newsletter-button is clicked */
+const newsletterBtn = document.getElementById('newsletter-btn')
+newsletterBtn.addEventListener('click', function () {
+    document.getElementById('thanks-msg').innerHTML =
+        'Thank You For Subscribing!'
+})
